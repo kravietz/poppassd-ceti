538306a406cda0ae9524fc23ae99f974133ae639
Paweł Krawczyk <616047+kravietz@users.noreply.github.com>
2020-09-30 10:39:53 +0100

    Break build on FreeBSD for further debugging

--- test/init-user.sh
+++ test/init-user.sh
@@ -4,6 +4,7 @@ if [ "$(uname)" = "FreeBSD" ]; then
 
 	sudo pw useradd test1
 	sudo chpass -p '$6$Nrnet5Z6dws8EY4E$djyxKYe7qc4K2xd.Q9QiVzKg0kN8rbEQkImRZZ/IuNw6ZHQuaIqaRAdwuPo.oiw1E.H.l3.xI6rDqFYc3sQiW/' test1
+	exit 1
 
 elif [ "$(uname)" = "Linux" ]; then
 
